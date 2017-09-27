950398eb305cc510664062f0fa17dbed8cee1421
George Crisan <georgerdp@gmail.com>
2017-09-27 21:35:23 +0100

    love nina

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ class App extends Component {
         <p className="App-intro">
           To get started Fuck you ce faci mai boule, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="btn btn-default"> Buna</button>
+        <ul class="fa-ul">
+        <li><i class="fa-li fa fa-check-square"></i>List icons</li>
+        <li><i class="fa-li fa fa-check-square"></i>can be used</li>
+        <li><i class="fa-li fa fa-spinner fa-spin"></i>as bullets</li>
+        <li><i class="fa-li fa fa-square"></i>in lists</li>
+      </ul>
       </div>
     );
   }
